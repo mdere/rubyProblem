ddd64c631002d0ec7a9e4528de76b084f377cbe5
Michael Dere <michael.dere@gmail.com>
2017-12-16 17:44:32 -0500

    backbone basic change_for done

--- currency.rb
+++ currency.rb
@@ -1,13 +1,19 @@
+require_relative('string.rb')
+
 class Currency
-  @currency_hash_mappings = {
-    us_coins: {
+  # Initialize 
+  
+  def initialize()
+    @currency_mappings = {}
+    @currency_mappings[:us_coins] = {
       quarters: 25,
       dimes: 10,
       nickels: 5,
       pennies: 1
     }
-  }
-  def initialize()
+    @selected_mapping = @currency_mappings[:us_coins]
+    @selected_mapping_mem_store = {}
+    create_mem_store()
   end
 
   # We will want to allow the users to be able to define
@@ -24,29 +30,70 @@ class Currency
   # This method should allow users to add mappings to a defined
   # list held in memory that takes in mapping_name <String>
   # mapping <Hash>
-  def self.set_new_currency_map(mapping_name, mapping)
-    begin
-      if mapping_name_is_valid(mapping_name) && mapping_is_valid(mapping)
-
-      end
-    rescue
-
+  def create_currency_mapping(mapping_name, mapping)
+    if mapping_name_is_valid(mapping_name) && mapping_is_valid(mapping)
+      this.currency_mappings[mapping_name] = mapping
     end
   end
 
-  def self.get_available_currency_map_names()
-
+  def set_mapping_for(map_name)
+    if mapping_exists(map_name)
+      @selected_mapping = nil
+      @selected_mapping = @currency_mappings[map_name]
+      create_mem_store()
+    end
   end
 
-  def self.get_currency_map_info(map_name)
-
+  def get_available_currency_map_names()
+    array_of_names = []
+    @currency_mappings.each do | key, value |
+      array_of_names.push(key)
+    end
+    return array_of_names
   end
 
-  def self.change_for(value)
+  def get_currency_map_info(map_name)
+    return @currency_mappings[map_name]
+  end
 
+  def change_for(value)
+    sanitizedValue = sanitize(value)
+    if sanitizedValue && sanitizedValue > 0
+      create_mem_store()
+      process_currency_for(value)
+    end
+    return @selected_mapping_mem_store[:currency_data]
   end
 
   private
+  def process_currency_for(value)
+    @selected_mapping_mem_store[:map_order].each do |mapping|
+      @selected_mapping_mem_store[:currency_data][mapping[0]] = value/mapping[1]
+      value = value%mapping[1]
+    end
+  end
+
+  def update_value(value)
+
+    return value
+  end
+
+  def create_mem_store()
+    # take @selected_mapping and build the mem_store
+    # start with currency_data
+    @selected_mapping_mem_store = {
+      currency_data: {},
+      map_order: []
+    }
+    puts @selected_mapping
+    @selected_mapping.each do |key, value|
+      @selected_mapping_mem_store[:currency_data][key] = 0
+      @selected_mapping_mem_store[:map_order].push([key, value])
+    end
+    # @selected_mapping_mem_store[:map_order].sort!{|x,y| y[1] <=> x[1]}
+    puts "current selected mem store #{@selected_mapping_mem_store}"
+  end
+
   def sanitize(value)
     # We will need to check if the string is
     # intended as an numerical
@@ -62,12 +109,18 @@ class Currency
     return nil
   end
 
-  def mapping_name_is_valid(mapping_name)
-
+  def mapping_exists(mapping_name)
+    return true
   end
 
-  def mapping_is_valid(mapping_value)
+  # for now I will only accept symbols
+  def mapping_name_is_valid(mapping_name)
+    mapping_name.class == Symbol
+  end
 
+  # Check if there are valid key and value
+  def mapping_is_valid(mapping_value)
+    return true
   end
 
 end
--- main.rb
+++ main.rb
@@ -1,58 +1,9 @@
-class String
-  def is_a_number?
-    Float(self) != nil rescue false
-  end
-end
+require_relative('currency.rb')
 
-def sanitize(value)
-  # We will need to check if the string is
-  # intended as an numerical
-  # Ignore floats for now
-  if value.is_a?(String)
-    return value.is_a_number? ? value.to_i : nil
-  end 
-  # Check if Interger already OR float
-  # Just floor the Float for now
-  if value.is_a?(Integer) || value.is_a?(Float)
-    return value.floor
-  end
-  return nil
-end
+currency = Currency.new
 
+current_mapping = currency.get_available_currency_map_names()
 
-def change_for(value)
-  sanitizedValue = sanitize(value)
-  if sanitizedValue && sanitizedValue > 0
-    # Init hash mem store
-    mem_store = {
-      quarters: 0,
-      dimes: 0,
-      nickels: 0,
-      pennies: 0
-    }
-    # quarters block
-    if sanitizedValue >= 25
-      mem_store[:quarters] = sanitizedValue/25
-      sanitizedValue = sanitizedValue%25
-    end
-    # dimes block
-    if sanitizedValue < 25 && sanitizedValue >= 10
-      mem_store[:dimes] = sanitizedValue/10
-      sanitizedValue = sanitizedValue%10
-    end
-    # nickels block
-    if sanitizedValue < 10 && sanitizedValue >= 5
-      mem_store[:nickels] = sanitizedValue/5
-      sanitizedValue = sanitizedValue%5
-    end
-    # pennies block
-    if sanitizedValue < 5 && sanitizedValue >= 1
-      mem_store[:pennies] = sanitizedValue/1
-      sanitizedValue = sanitizedValue%1
-    end
-  end
-  puts mem_store
-  return mem_store
-end
+puts "current available mappings: #{current_mapping}" 
 
-change_for(9999)
+puts currency.change_for(55)
